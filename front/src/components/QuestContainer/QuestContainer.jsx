ae3956784f9019373ff3d385744aaa4b5fa650c7
Santiago Paiz <paizsantiago5@gmail.com>
2023-08-23 14:30:31 -0300

    corrigiendo data

--- front/src/components/QuestContainer/QuestContainer.jsx
+++ front/src/components/QuestContainer/QuestContainer.jsx
@@ -1,38 +1,46 @@
-import React, {useEffect, useState, useContext} from 'react'
-import data from '../../data/preguntas.json'
-import { Quest } from '../Quest/Quest';
-import { ContextPrueba } from '../../context/ContextPrueba';
-import { Result } from '../Quest/Result';
+import React, { useEffect, useState, useContext } from "react";
+import data from "../../data/preguntas.json";
+import { Quest } from "../Quest/Quest";
+import { ContextPrueba } from "../../Context/ContextPrueba";
+import { Result } from "../Restult/Result";
 
 export const QuestContainer = () => {
+  const { select, resultadoQuest } = useContext(ContextPrueba);
+  const [quest, setQuest] = useState([]);
+  const [testComplete, setTestComplete] = useState(false);
 
-    const {select, resultadoQuest} = useContext(ContextPrueba);
-    const [quest, setQuest] = useState([]);
-    const [testComplete, setTestComplete] = useState(false);
+  useEffect(() => {
+    setQuest(data.test);
+  }, []);
 
-    useEffect(() => {
-      setQuest(data.test)
-    }, [])
-
-    
-    const submitChange = (event) =>{
-        event.preventDefault();
-        setQuest([]);
-        for (const item of select) {
-            resultadoQuest[item.area-1] += +1;
-        }
-        setTestComplete(true)
-        console.log(resultadoQuest)
+  const submitChange = (event) => {
+    event.preventDefault();
+    setQuest([]);
+    for (const item of select) {
+      resultadoQuest[item.area - 1] += 1;
     }
+    setTestComplete(true);
+    console.log(resultadoQuest);
+  };
 
-
-    return (
+  return (
+    <div>
+      <form>
+        {quest.map((item) => (
+          <Quest quest={item} />
+        ))}
+        {!testComplete ? <input type="submit" onClick={submitChange} /> : <></>}
+      </form>
+      {testComplete ? (
         <div>
-            <form>
-                {quest.map((item) => <Quest quest={item}/>)}
-                {!testComplete ? <input type="submit" onClick={submitChange}/> : <></>}
-            </form>
-            {testComplete ? <div><h2>Terminaste el quest, tus resultados son: {<Result result={resultadoQuest}/>}</h2></div> : <></>}
+          <h2>
+            Terminaste el quest, tus resultados son:
+            {<Result result={resultadoQuest} />}
+          </h2>
         </div>
-    )
-}
+      ) : (
+        <></>
+      )}
+    </div>
+  );
+};
